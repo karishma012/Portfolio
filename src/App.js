bd2be7e9e2911e9e323df6dd68d7b577f5260795
karishma012 <115379019+karishma012@users.noreply.github.com>
2023-11-15 01:03:41 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import About from './Components/About';
-import starsBackground from './Components/stars3.avif'; // Import the background image
 import Skills from './Components/Skills';
 import ProjectsSection from './Components/ProjectsSection';
 import Footer from './Components/Footer';
 import { Element } from "react-scroll";
 
 
+
 function App() {
-  const backgroundStyle = {
-    backgroundImage: `url(${starsBackground})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    minHeight: '100vh', // Adjust as needed for your layout
-  };
+  
 
   return (
     <div className='bg-gray-950'>
@@ -37,6 +31,7 @@ function App() {
       <Element name="Contact" className="section">
       <Footer/>
       </Element>
+      
       
     </div>
   );
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,46 +1,61 @@
-// Footer.js
-
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faInstagram, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { motion, useAnimation } from 'framer-motion';
+import { motion } from 'framer-motion';
+import ninja from './cnn.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-950 text-white py-8">
-      
+    <footer className="bg-black text-white py-16 sm:py-32 md:py-40 lg:py-60">
+      <div className="flex justify-center mb-8">
+        {/* Social media icons */}
+        <a href="https://www.linkedin.com/in/kk1210/" target="_blank" rel="noopener noreferrer" className="mx-6 transform hover:scale-110 transition-transform">
+          <FontAwesomeIcon icon={faLinkedin} size="3x" />
+        </a>
+        <a href="https://twitter.com/KhemnaniQ" target="_blank" rel="noopener noreferrer" className="mx-6 text-blue-400 transform hover:scale-110 transition-transform">
+          <FontAwesomeIcon icon={faTwitter} size="3x" />
+        </a>
+        <a href="https://github.com/karishma012" target="_blank" rel="noopener noreferrer" className="mx-6 transform hover:scale-110 transition-transform">
+          <FontAwesomeIcon icon={faGithub} size="3x" />
+        </a>
+      </div>
       <motion.div
         initial={{ x: '-100%' }}
         animate={{ x: '0%' }}
         transition={{ duration: 1.5 }}
-        className="text-center text-gray-700 p-8 text-3xl font-bold animate-pulse"
+        className="text-center text-white p-4 text-4xl font-bold"
       >
         Contact Me
       </motion.div>
-      <div className="flex justify-center mb-8">
-        {/* Social media icons */}
-        <a href="https://www.linkedin.com/in/kk1210/" target="_blank" rel="noopener noreferrer" className="mx-6 text-blue-800 transform hover:scale-110 transition-transform">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" />
-        </a>
-        <a href="https://www.instagram.com/k.pvt01210/" target="_blank" rel="noopener noreferrer" className="mx-6 bg-pink-900 transform hover:scale-110 transition-transform">
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
-        </a>
-        <a href="https://twitter.com/KhemnaniQ" target="_blank" rel="noopener noreferrer" className="mx-6 text-blue-500 transform hover:scale-110 transition-transform">
-          <FontAwesomeIcon icon={faTwitter} size="2x" />
-        </a>
-        <a href="https://github.com/karishma012" target="_blank" rel="noopener noreferrer" className="mx-6 transform hover:scale-110 transition-transform">
-          <FontAwesomeIcon icon={faGithub} size="2x" />
-        </a>
-      </div>
-
-      <div className="text-center mt-10">
+      <div className="text-center">
         {/* Email icon and address */}
         <div className="flex items-center justify-center">
-          <FontAwesomeIcon icon={faEnvelope} size="lg" className="mr-4 text-red-700" />
-          <span className='text-gray-500'>khemnanikarishma111@gmail.com</span>
+          <FontAwesomeIcon icon={faEnvelope} size="lg" className="mr-4 text-red-700 text-xl" />
+          <span className='text-white text-2xl'>khemnanikarishma111@gmail.com</span>
         </div>
       </div>
+      <div className="text-center mt-4">
+        {/* Email icon and address */}
+        <div className="flex items-center justify-center text-orange-800 text-3xl font-semibold">
+          <h1>Coding Profiles</h1>
+        </div>
+      </div>
+      <div className="flex justify-center mt-6 mx-10 px-8">
+        <a href="https://auth.geeksforgeeks.org/user/khemnanikarishma111" target="_blank" rel="noopener noreferrer">
+          <img src="https://img.icons8.com/color/480w/GeeksforGeeks.png" alt="GeeksforGeeks" className="h-8 w-8 mx-6 cursor-pointer transform hover:scale-150 transition-transform" />
+        </a>
+        <a href="https://www.codingninjas.com/studio/profile/Karishma12" target="_blank" rel="noopener noreferrer">
+          <img src={ninja} alt="Coding Ninjas" className="h-10 w-10 mx-6 cursor-pointer  transform hover:scale-150 transition-transform" />
+        </a>
+        <a href="https://www.hackerrank.com/profile/khemnanikarishm1" target="_blank" rel="noopener noreferrer">
+          <img src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/160_Hackerrank_logo_logos-512.png" alt="HackerRank" className="h-8 w-8 mx-6 cursor-pointer transform hover:scale-150 transition-transform" />
+        </a>
+        <a href="https://leetcode.com/karishma-12/" target="_blank" rel="noopener noreferrer">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/8/8e/LeetCode_Logo_1.png" alt="LeetCode" className="h-8 w-8 mx-6 cursor-pointer transform hover:scale-150 transition-transform" />
+        </a>
+      </div>
+      <p className='text-center text-gray-500 mt-16 text-xl'>Copyright ©2023 All rights reserved</p>
     </footer>
   );
 };
